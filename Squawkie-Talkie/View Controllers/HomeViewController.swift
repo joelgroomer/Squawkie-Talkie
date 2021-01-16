e9f092e20b859d55d46167fd62877ee94b20d546
Aaron Haughton <dreamcast2000@outlook.com>
2021-01-16 16:51:04 -0600

    Created HomeViewController

---
+++ Squawkie-Talkie/View Controllers/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  Squawkie-Talkie
+//
+//  Created by Joe on 1/16/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+//     MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
+}
