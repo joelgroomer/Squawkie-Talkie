ae9695bef15ac72ab2a40e96f64d6242e439073a
Aaron Haughton <dreamcast2000@outlook.com>
2021-01-17 12:36:44 -0600

    Setup Items on ParrotDetailView

---
+++ Squawkie-Talkie/View Controllers/ParrotDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ParrotDetailViewController.swift
+//  Squawkie-Talkie
+//
+//  Created by Joe on 1/17/21.
+//
+
+import UIKit
+
+class ParrotDetailViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
