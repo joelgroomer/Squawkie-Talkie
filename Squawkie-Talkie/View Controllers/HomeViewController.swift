99fc99d299b0f4795d0bfd7e261c3f69fa108f1f
Aaron Haughton <dreamcast2000@outlook.com>
2021-01-17 00:25:09 -0600

    commit before merge

--- Squawkie-Talkie/View Controllers/HomeViewController.swift
+++ Squawkie-Talkie/View Controllers/HomeViewController.swift
@@ -46,6 +46,7 @@ class HomeViewController: UIViewController {
         tipsTableView.dataSource = self
         tipsTableView.delegate = self
     }
+    
     @IBAction func sampleDataButtonTapped(_ sender: UIButton) {
         do {
             try dataController.createSampleData()
@@ -88,67 +89,14 @@ extension HomeViewController: NSFetchedResultsControllerDelegate {
     }
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         newsTableView.endUpdates()
-
-}
-extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return frc.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-    }
-}
-
-extension HomeViewController: NSFetchedResultsController<NSFetchRequestResult> {
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        newsTableView.beginUpdates()
-        tipsTableView.beginUpdates()
-    }
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        newsTableView.endUpdates()
-        tipsTableView.endUpdates()
-
     }
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
         switch type {
         case .insert:
-
             newsTableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
         case .delete:
             newsTableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
         default: break
         }
     }
-
-            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
-        case .delete:
-            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
-        default: break
-        }
-    }
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            guard let newIndexPath = newIndexPath else { return }
-            tipsTableView.insertRows(at: [newIndexPath], with: .automatic)
-            newsTableView.insertRows(at: [newIndexPath], with: .automatic)
-        case .update:
-            guard let indexPath = indexPath else { return }
-            tipsTableView.reloadRows(at: [indexPath], with: .automatic)
-            newsTableView.reloadRows(at: [indexPath], with: .automatic)
-        case .move:
-            guard let oldIndexpath = indexPath, let newIndexPath = newIndexPath else { return }
-            tipsTableView.deleteRows(at: [oldIndexpath], with: .automatic)
-            newsTableView.deleteRows(at: [oldIndexpath], with: .automatic)
-            tipsTableView.insertRows(at: [newIndexPath], with: .automatic)
-            newsTableView.insertRows(at: [newIndexPath], with: .automatic)
-        case .delete:
-            guard let indexPath = indexPath else { return }
-            tipsTableView.deleteRows(at: [indexPath], with: .automatic)
-            newsTableView.deleteRows(at: [indexPath], with: .automatic)
-        @unknown default: break
-        }
-    }
-
 }
