8cb6351bc0242b0cd329373e88fa3da62cb71724
Aaron Haughton <dreamcast2000@outlook.com>
2021-01-17 20:47:22 -0600

    Create MyFlockCollectionView File; updated Storyboard with CollectionView Controller.

---
+++ Squawkie-Talkie/View Controllers/MyFlockCollectionViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MyFlockCollectionViewController.swift
+//  Squawkie-Talkie
+//
+//  Created by Joe on 1/17/21.
+//
+
+import UIKit
+
+private let reuseIdentifier = "MyFlockCell"
+
+class MyFlockCollectionViewController: UICollectionViewController {
+    
+    let dataController = DataController()
+    
+    var parrots: [Parrot] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToDetail" {
+            if let indexPath = sender as? IndexPath, let vc = segue.destination as? ParrotDetailViewController {
+                vc.parrot = parrots[indexPath.row]
+            }
+        }
+    }
+   
+
+    // MARK: UICollectionViewDataSource
+
+//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return parrots.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToDetail", sender: indexPath)
+    }
+
+}
---
+++ Squawkie-Talkie/Views/MyFlockCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyFlockCollectionViewCell.swift
+//  Squawkie-Talkie
+//
+//  Created by Joe on 1/17/21.
+//
+
+import UIKit
+
+class MyFlockCollectionViewCell: UICollectionViewCell {
+    
+}
